04247808122d7a1b11b493ac69b88000033cbe47
Tristan Dunn <tristanzdunn@gmail.com>
2009-02-01 13:05:02 -0600

    Require authentication by default.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
+  include Authentication
+
   # Include all helpers, all the time.
   helper :all
+
+  # Require authentication by default.
+  before_filter :login_required
 
   # See ActionController::RequestForgeryProtection for details.
   protect_from_forgery
