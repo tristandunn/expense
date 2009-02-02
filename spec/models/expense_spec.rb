4eaeeac628e4b56c2b489530b25cbaba904652f6
Tristan Dunn <tristanzdunn@gmail.com>
2009-02-01 20:21:09 -0600

    Use my timezone.

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Expense do
   before do
-    Time.zone = 'America/Chicago'
+    Time.zone = 'Central Time (US & Canada)'
   end
 
   describe 'class' do
