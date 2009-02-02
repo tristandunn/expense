1491f100743a2693e48cd35dc441e36fa561dfbe
Tristan Dunn <tristanzdunn@gmail.com>
2009-02-01 19:53:51 -0600

    Add a logout link.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :expenses
   map.resource  :session
   map.resources :users, :member => { :delete => :get }
+  map.logout    'logout', :controller => 'sessions', :action => 'destroy'
   map.root      :controller => 'expenses'
 end
