80fff97e9d3d3c2b9cce4a1e9c17e4d946221de3
Tristan Dunn <tristanzdunn@gmail.com>
2009-04-26 13:56:36 -0500

    Add support for iPhone specific responses.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,22 @@ class ApplicationController < ActionController::Base
   # Require authentication by default.
   before_filter :login_required
 
+  # Adjust the request form for iPhone requests.
+  before_filter :adjust_format_for_iphone
+
   # See ActionController::RequestForgeryProtection for details.
   protect_from_forgery
 
   # Scrub sensitive parameters from your log.
   filter_parameter_logging :password, :password_confirmation
+
+  protected
+
+  def adjust_format_for_iphone
+    request.format = :iphone if iphone_request?
+  end
+
+  def iphone_request?
+    return request.env['HTTP_USER_AGENT'] && request.env['HTTP_USER_AGENT'][/(Mobile\/.+Safari)/]
+  end
 end
