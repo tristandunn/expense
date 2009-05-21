756eabfbce4d1c539cead622635f6eb2e8d75352
Tristan Dunn <tristanzdunn@gmail.com>
2009-05-20 22:36:34 -0500

    Extract sidebar out into partial for sharing with search view.

--- spec/views/expenses/index_spec.html.rb
+++ spec/views/expenses/index_spec.html.rb
@@ -5,13 +5,12 @@ describe 'when rendering /expenses/index.html' do
     @today     = mock_model(Expense, :cost => 1,  :item => 'a lottery ticket')
     @yesterday = mock_model(Expense, :cost => 10, :item => 'a big bag of chips')
 
-    assigns[:averages] = {}
     assigns[:groups]   = [
       ['Today',     [@today]],
       ['Yesterday', [@yesterday]]
     ]
 
-    template.stub!(:class_for_status_of)
+    template.should_receive(:render).with(:partial => 'sidebar')
   end
 
   it' should render expenses in groups' do
@@ -27,36 +26,6 @@ describe 'when rendering /expenses/index.html' do
     response.should have_tag('li') do
       with_tag 'strong', '$10.00'
       with_tag 'span',   'a big bag of chips'
-    end
-  end
-
-  describe 'when averages are present' do
-    before do
-      assigns[:averages] = {
-        :day   => 1,
-        :week  => 2,
-        :month => 3
-      }
-
-      template.stub!(:class_for_status_of).and_return('below')
-    end
-
-    it 'should render averages for day, week and month with class name of status' do
-      render 'expenses/index.html.erb'
-
-      response.should have_tag('dd[class=?]', 'below', '$1.00')
-      response.should have_tag('dd[class=?]', 'below', '$2.00')
-      response.should have_tag('dd[class=?]', 'below', '$3.00')
-    end
-  end
-
-  describe 'when no averagaes are present' do
-    it 'should render $0.00 for each' do
-      render 'expenses/index.html.erb'
-
-      response.should have_tag('dl') do |elements|
-        with_tag 'dd', '$0.00', :count => 3
-      end
     end
   end
 end
