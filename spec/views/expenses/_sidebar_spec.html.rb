1cbcb4b0af6ac1c9c3d28d9aa02bd46fce0141ea
Tristan Dunn <tristanzdunn@gmail.com>
2009-05-20 22:38:23 -0500

    Add search form to sidebar.

--- spec/views/expenses/_sidebar_spec.html.rb
+++ spec/views/expenses/_sidebar_spec.html.rb
@@ -1,6 +1,20 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe 'when rendering /expenses/_sidebar.html' do
+  before do
+    assigns[:averages] = {}
+
+    template.stub!(:class_for_status_of)
+  end
+
+  it 'should render search form in sidebar' do
+    render 'expenses/_sidebar.html.erb'
+
+    response.should have_tag('form[action=?][method=?]', search_expenses_path, 'get') do
+      with_tag 'input[name=?]', 'search[query]'
+    end
+  end
+
   describe 'when averages are present' do
     before do
       assigns[:averages] = {
@@ -22,12 +36,6 @@ describe 'when rendering /expenses/_sidebar.html' do
   end
 
   describe 'when no averagaes are present' do
-    before do
-      assigns[:averages] = {}
-
-      template.stub!(:class_for_status_of)
-    end
-
     it 'should render $0.00 for each' do
       render 'expenses/_sidebar.html.erb'
 
