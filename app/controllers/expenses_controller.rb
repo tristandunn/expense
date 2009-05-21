56521e250ab8636bb4881399e8c310fd0cf31ab5
Tristan Dunn <tristanzdunn@gmail.com>
2009-05-20 22:20:30 -0500

    Move common queries to before filters in preparation for searching.

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,14 +1,11 @@
 class ExpensesController < ApplicationController
+  before_filter :find_expenses,      :only => %w(index)
+  before_filter :build_expense,      :only => %w(index)
+  before_filter :calculate_averages, :only => %w(index)
+
   # List recent expenses.
   def index
-    @expense  = current_user.expenses.build
-    @expenses = current_user.expenses
-    @groups   = @expenses.find_recent_grouped_by_relative_date
-    @averages = {
-      :day   => @expenses.calculate_average_for(:day),
-      :week  => @expenses.calculate_average_for(:week),
-      :month => @expenses.calculate_average_for(:month)
-    }
+    @groups = @expenses.find_recent_grouped_by_relative_date
   end
 
   # Display new expense form.
@@ -31,5 +28,26 @@ class ExpensesController < ApplicationController
         format.iphone { redirect_to '/' }
       end
     end
+  end
+
+  protected
+
+  # Build an expense for the current user.
+  def build_expense
+    @expense = current_user.expenses.build
+  end
+
+  # Calculate daily, weekly and monthly averages.
+  def calculate_averages
+    @averages = {
+      :day   => @expenses.calculate_average_for(:day),
+      :week  => @expenses.calculate_average_for(:week),
+      :month => @expenses.calculate_average_for(:month)
+    }
+  end
+
+  # Find current users expenses.
+  def find_expenses
+    @expenses = current_user.expenses
   end
 end
