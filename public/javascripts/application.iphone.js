89b5f9c086a20ddd8da9ee79f0042943a325a445
Tristan Dunn <tristanzdunn@gmail.com>
2010-07-05 13:46:14 -0400

    Use HTML5 placeholders instead of JavaScript.

--- public/javascripts/application.iphone.js
+++ public/javascripts/application.iphone.js
@@ -1,15 +1,7 @@
 window.onload = function() {
-  setTimeout(function() {
-    window.scrollTo(0, 1);
-  }, 100);
-
-  var item = document.getElementById('payment_item');
-
-  if (item) {
-    item.value   = 'Example: 5.45 on Subway for lunch';
-    item.onfocus = function() {
-      this.value   = '';
-      this.onfocus = null;
-    };
+  if (!window.navigator.standalone) {
+    setTimeout(function() {
+      window.scrollTo(0, 1);
+    }, 100);
   }
 };
