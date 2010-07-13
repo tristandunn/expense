00f97d4befb6a6fd3d19ed2c5cae2026d4914cd9
Tristan Dunn <tristanzdunn@gmail.com>
2010-07-12 20:15:54 -0400

    Use UTC instead of Eastern Time.

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,8 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Expense
   class Application < Rails::Application
-    config.encoding  = 'utf-8'
-    config.time_zone = 'Eastern Time (US & Canada)'
+    config.encoding  = 'UTF-8'
+    config.time_zone = 'UTC'
 
     config.filter_parameters += [:password, :password_confirmation]
   end
