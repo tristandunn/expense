850fdde08e189c9552bd0f87d762e65905503ef3
Tristan Dunn <tristanzdunn@gmail.com>
2010-07-15 21:15:34 -0400

    Restrict routes to used actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Expense::Application.routes.draw do |map|
-  resources :payments
-  resource  :session
+  resources :payments, :only => [:index, :new, :create]
+  resource  :session,  :only => [:new, :create, :destroy]
 
-  resources :users do
+  resources :users, :only => [:new, :create, :destroy] do
     member do
       get :delete
     end
