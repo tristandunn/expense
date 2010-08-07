ace5817b2c3c13a552a1f88843a2109f9018a614
Tristan Dunn <tristanzdunn@gmail.com>
2010-08-06 22:59:16 -0400

    Remove unused helper and matcher.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,22 +11,4 @@ RSpec.configure do |config|
   config.mock_with :rspec
   config.fixture_path = ::Rails.root.join('spec', 'fixtures')
   config.use_transactional_fixtures = true
-
-  def user_is_valid
-    controller.stub!(:current_user).and_return(@user = mock_model(User))
-  end
-end
-
-RSpec::Matchers.define :assign_to do |key, expected|
-  match do
-    assigns(key).should == expected
-  end
-
-  failure_message_for_should do
-    "expected #{expected.inspect} to be assigned to @#{key}"
-  end
-
-  failure_message_for_should_not do
-    "expected #{expected.inspect} to not be assigned to @#{key}"
-  end
 end
